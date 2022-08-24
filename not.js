aa9032bef807e197538591c0500520dcb28eaf47
symakbs25 <111290671+symakbs25@users.noreply.github.com>
2022-08-24 10:13:47 +0300

    Add files via upload

---
+++ not.js
@@ -0,0 +1,44 @@
+function notHesapla() {
+    var vize1 = Number(document.getElementById("vize1").value);
+    var vize2 = Number(document.getElementById("vize2").value);
+    var final = Number(document.getElementById("final").value);
+    var puan = (vize1 + vize2 + final) / 3;
+  
+    
+    if (puan >= 90) {
+      var harf = "AA";
+    } else if (puan >= 80) {
+      var harf = "BB";
+    } else if (puan >= 70) {
+      var harf = "CB";
+    } else if (puan >= 60) {
+      var harf = "CC";
+    } else {
+      var harf = "FF";
+    }
+  
+    document.getElementById("sonuc").innerHTML =
+      "Sayın " +
+      localStorage.getItem("isim") +
+      " " +
+      localStorage.getItem("soyisim")+
+      " harf notunuz: " +
+      harf;
+  }
+  
+  function kaydet(){
+    var isim = document.getElementById("isim").value;
+    var soyisim = document.getElementById("soyisim").value;
+    localStorage.setItem('isim', isim);
+    localStorage.setItem('soyisim', soyisim);
+    
+  }
+  
+  function sayfa1(){
+    window.location.assign('index.html');
+  }
+  
+  function sayfa2(){
+  
+    window.location.assign('sayfa2.html');
+  }
